224f5a47d5b6b0f60b0fc995a874d169a1b909b7
gk199 <gillian.kopp@gmail.com>
2023-04-13 17:07:09 +0200

    plotting energy profiles overlay

--- Plot/KinematicPlots.py
+++ Plot/KinematicPlots.py
@@ -29,21 +29,26 @@ ypos = 0.85
 
 canv = ROOT.TCanvas("c","c",800,600)
 overlayCanv = ROOT.TCanvas("c1","c1",800,600)
-if (data): cmsLabel = "#scale[1.0]{#bf{CMS}} #scale[0.8]{#it{2022 13.6 TeV Collisions Data}}"; 
-if (MC): cmsLabel = "#scale[1.0]{#bf{CMS}} #scale[0.8]{#it{2023 LLP MC}}"; 
-print("Starting plotting script")
+if (data): 
+  cmsLabel = "#scale[1.0]{#bf{CMS}} #scale[0.8]{#it{2022 13.6 TeV Collisions Data}}"; 
+  folder = "./outPlots/"
+if (MC): 
+  cmsLabel = "#scale[1.0]{#bf{CMS}} #scale[0.8]{#it{2023 LLP MC}}"; 
+  folder = "./outPlots_MC/"
 
 # Save output plots
-folder = "./outPlots/"
-if (MC): folder = "./outPlots_MC/"
-try:
-  os.makedirs(folder)
-except OSError:
-  pass 
+def OutputFolder(name):
+  try:
+    os.makedirs(folder + name)
+  except OSError:
+    pass 
+
+OutputFolder("")
 
 path = sys.argv[1] # pass the location of the processed ntuple as an argument 
-
 infile = ROOT.TFile.Open( path)
+
+print("Starting plotting script")
 
 categories = {"NoSel", "JetPt40", "PassedHLT"};
 objects = {"jet", "ele", "muon", "pho"}
@@ -52,10 +57,7 @@ leading = [0,1,2]
 
 print("Making plots!")
 for obj in objects:
-  try:
-    os.makedirs(folder + obj)
-  except OSError:
-    pass
+  OutputFolder(obj)
   for quant in quantities:
     for cat in categories:
       legend = ROOT.TLegend(xpos+0.2,0.65,xpos+0.35,0.8)
@@ -87,16 +89,16 @@ for obj in objects:
           overlayCanv.SaveAs(folder + obj + "/Overlay_" + obj + str(i) + "_" + quant + "_" + cat + ".png")
 
 if (MC):
-  plots = {"gen_Ddecay", "gen_Rdecay", "gen_Xdecay", "gen_Ydecay", "gen_Zdecay", "gen_cTau", "gen_deltaT", "gen_rechitNpt2", "gen_rechitNpt4", "gen_rechitNpt6", "gen_energyP"};
+  plots = {"gen_Ddecay", "gen_Rdecay", "gen_Xdecay", "gen_Ydecay", "gen_Zdecay", "gen_cTau", "gen_deltaT"};
 
-  print ("Plots for generator LLP information now run: ")
+  print ("Plots for generator LLP positional information now run: ")
   for plot in plots:
     print (plot)
     legend = ROOT.TLegend(xpos+0.2,0.65,xpos+0.35,0.8)
     canv.cd()
     hist = infile.Get(plot)
     if (hist.GetEntries() > 0): hist.Scale(1/hist.GetEntries())
-    if (plot == "gen_deltaT" or plot == "gen_rechitNpt2" or plot == "gen_rechitNpt4" or plot == "gen_rechitNpt6" or plot == "gen_energyP"): canv.SetLogy()
+    if (plot == "gen_deltaT"): canv.SetLogy()
     else: canv.SetLogy(0)
 
     if (plot == "gen_cTau"):
@@ -117,9 +119,50 @@ if (MC):
       hist.Draw("HIST PLC")
     stamp_text.DrawLatex( xpos+0.2, ypos, cmsLabel)
 
-    try:
-      os.makedirs(folder + "genLLP")
-    except OSError:
-      pass
+    OutputFolder("genLLP")
     canv.SaveAs(folder + "genLLP" + "/" + plot + ".png")
 
+  print ("Plots for generator LLP rechit multiplicity and energy profiles now run: ")
+  plots = {"gen_rechitNpt2", "gen_rechitNpt4", "gen_rechitNpt6", "gen_energyP"}
+  leading = [0,1,2]
+  for plot in plots:
+    legend = ROOT.TLegend(xpos+0.2,0.65,xpos+0.35,0.8)
+    for i in leading:
+      type = "unknown"
+      if (i == 0): type = " LLP"
+      if (i == 1): type = " daughter 1"
+      if (i == 2): type = " daughter 2"
+      canv.cd()
+      hist = infile.Get(plot + "_" + str(i))
+      if (hist.GetEntries() > 0): hist.Scale(1/hist.Integral(0,5))
+      if (plot[0:10] == "gen_rechit"): canv.SetLogy()
+      else: canv.SetLogy(0)
+      legend.AddEntry(hist,plot[4:0] + type)
+
+      # Overlay quantities now
+      if (i == 0): hist.Draw("HIST PLC")
+      if (i > 0 ): hist.Draw("SAME HIST PLC")
+      if (i == 2): 
+          legend.Draw()
+          stamp_text.DrawLatex(xpos+0.2, ypos, cmsLabel)
+          canv.SaveAs(folder + "genLLP" + "/Overlay_" + plot + ".png")
+
+  print ("2D histogram for rechit vs. LLP depth; fractional energy vs. HCAL depth")
+  plots = {"gen_depth_energyP", "gen_decay_rechitN"}  
+  leading = [0,1,2]
+  for plot in plots:
+    for i in leading:
+      legend = ROOT.TLegend(xpos+0.2,0.65,xpos+0.35,0.8)
+      type = "unknown"
+      if (i == 0): type = " LLP"
+      if (i == 1): type = " daughter 1"
+      if (i == 2): type = " daughter 2"
+      canv.cd()
+      hist = infile.Get(plot + "_" + str(i))
+      canv.SetLogy(0)
+      canv.SetLogz()
+      legend.AddEntry(hist,plot[10:0] + type)
+      hist.Draw("COLZ")
+      legend.Draw()
+      stamp_text.DrawLatex(xpos+0.2, ypos, cmsLabel)
+      canv.SaveAs(folder + "genLLP" + "/" + plot + "_" + type[1:-2] + type[-1] + ".png")
