27051eab502c7baa7d34eaeacec03a1d7079a95f
gk199 <gillian.kopp@gmail.com>
2023-12-08 13:46:52 +0100

    option to loop over different pairs of jet E and event HT selections

--- MiniTuplePlotter/Gillian/MiniTuplePlotter_HLT_Effs.C
+++ MiniTuplePlotter/Gillian/MiniTuplePlotter_HLT_Effs.C
@@ -32,26 +32,32 @@ void MiniTuplePlotter_HLT_Effs(){
 
 	vector<string> filetag_keys_to_loop = {"LLP125", "LLP350"};
 
+	vector<string> jet_E = {"60", "100"};
+	vector<string> event_HT = {"200", "170"};
+
 	for( auto key: filetag_keys_to_loop){
 
-		// with a jet pT cut, plot efficiency vs LLP displacement in R, Z, eta, travel time
-		class MiniTuplePlotter eff_LLPdisplacement( filetags[key], path );
-		eff_LLPdisplacement.SetPlots({P_LLP0_DecayR, P_LLP0_DecayZ, P_LLP0_Eta, P_LLP0_TravelTime});
-		eff_LLPdisplacement.SetTreeName( "NoSel" );
-		eff_LLPdisplacement.SetOutputFileTag("HLT_v3_MC_"+key);
-		eff_LLPdisplacement.SetOutputDirectory("HLT_Efficiencies");
-		eff_LLPdisplacement.plot_norm 			= false;
-		eff_LLPdisplacement.plot_log_ratio   	= true;
-		eff_LLPdisplacement.SetLegendPosition( 0.6, 0.7, 0.88, 0.88 );
-		eff_LLPdisplacement.SetSelectiveCuts("MC", "eventHT > 170 && ( (jet0_isMatchedTo == 0 && jet0_Pt >= 100) || (jet1_isMatchedTo == 0 && jet1_Pt >= 100) || (jet2_isMatchedTo == 0 && jet2_Pt >= 100) ) "); // make sure that LLP 0 is matched to jet, and cut on the jet pT
-		eff_LLPdisplacement.SetComparisonCuts({Cut_None, Cut_HLTpassed1, Cut_AnyLLP_HLT}); 
-		eff_LLPdisplacement.SetLegendNames({"No cuts", "Monitoring HLT passed", "LLP HLT passed"});
-		eff_LLPdisplacement.Plot("ratio");
-		eff_LLPdisplacement.ClearFileTrees(); 														// reset, and cut on each HLT group
-		eff_LLPdisplacement.SetOutputFileTag("HLT_v3_MC_"+key+"_HLTsplit");
-		eff_LLPdisplacement.SetComparisonCuts({Cut_HLTpassed1, Cut_AnyLLP_HLT, Cut_HLTpassed2, Cut_HLTpassed9, Cut_HLTpassed5, Cut_HLTpassed11}); 
-		eff_LLPdisplacement.SetLegendNames({"Monitoring HLT passed", "LLP HLT passed", "Displaced dijet, <=1 prompt track", "Displaced dijet, <=2 prompt tracks", "Displaced dijet, 1 displaced track", "Delayed jet, ECAL jet timing"});
-		eff_LLPdisplacement.Plot("ratio");
+		for( int i = 0; i < jet_E.size(); i++ ){
+
+			// with a jet pT cut, plot efficiency vs LLP displacement in R, Z, eta, travel time
+			class MiniTuplePlotter eff_LLPdisplacement( filetags[key], path );
+			eff_LLPdisplacement.SetPlots({P_LLP0_DecayR, P_LLP0_DecayZ, P_LLP0_Eta, P_LLP0_TravelTime});
+			eff_LLPdisplacement.SetTreeName( "NoSel" );
+			eff_LLPdisplacement.SetOutputFileTag("HLT_v3_MC_"+key+"_jetE"+jet_E[i]);
+			eff_LLPdisplacement.SetOutputDirectory("HLT_Efficiencies");
+			eff_LLPdisplacement.plot_norm 			= false;
+			eff_LLPdisplacement.plot_log_ratio   	= true;
+			eff_LLPdisplacement.SetLegendPosition( 0.6, 0.7, 0.88, 0.88 );
+			eff_LLPdisplacement.SetSelectiveCuts("MC", Form("eventHT > %s && ( (jet0_isMatchedTo == 0 && jet0_Pt >= %s) || (jet1_isMatchedTo == 0 && jet1_Pt >= %s) || (jet2_isMatchedTo == 0 && jet2_Pt >= %s) ) ", event_HT[i].c_str(), jet_E[i].c_str(), jet_E[i].c_str(), jet_E[i].c_str() ) ); // make sure that LLP 0 is matched to jet, and cut on the jet pT
+			eff_LLPdisplacement.SetComparisonCuts({Cut_None, Cut_HLTpassed1, Cut_AnyLLP_HLT}); 
+			eff_LLPdisplacement.SetLegendNames({"No cuts", "Monitoring HLT passed", "LLP HLT passed"});
+			eff_LLPdisplacement.Plot("ratio");
+			eff_LLPdisplacement.ClearFileTrees(); 														// reset, and cut on each HLT group
+			eff_LLPdisplacement.SetOutputFileTag("HLT_v3_MC_"+key+"_jetE"+jet_E[i]+"_HLTsplit");
+			eff_LLPdisplacement.SetComparisonCuts({Cut_HLTpassed1, Cut_AnyLLP_HLT, Cut_HLTpassed2, Cut_HLTpassed9, Cut_HLTpassed5, Cut_HLTpassed11}); 
+			eff_LLPdisplacement.SetLegendNames({"Monitoring HLT passed", "LLP HLT passed", "Displaced dijet, <=1 prompt track", "Displaced dijet, <=2 prompt tracks", "Displaced dijet, 1 displaced track", "Delayed jet, ECAL jet timing"});
+			eff_LLPdisplacement.Plot("ratio");
+		}
 
 		// cut on LLP displacement, plot efficiency vs jet pT, E, eta, phi, event HTT
 		class MiniTuplePlotter plotter_HLTeffMC( filetags[key], path );
@@ -71,6 +77,7 @@ void MiniTuplePlotter_HLT_Effs(){
 		plotter_HLTeffMC.SetComparisonCuts({Cut_HLTpassed1, Cut_AnyLLP_HLT, Cut_HLTpassed2, Cut_HLTpassed9, Cut_HLTpassed5, Cut_HLTpassed11}); 
 		plotter_HLTeffMC.SetLegendNames({"Monitoring HLT passed", "LLP HLT passed", "Displaced dijet, <=1 prompt track", "Displaced dijet, <=2 prompt tracks", "Displaced dijet, 1 displaced track", "Delayed jet, ECAL jet timing"});
 		plotter_HLTeffMC.Plot("ratio");
+
 	}
 
 	if (September) {
