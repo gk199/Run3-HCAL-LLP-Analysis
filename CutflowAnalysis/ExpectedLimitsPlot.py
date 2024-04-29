101cdc5c7d2ff7c5e7c52464d48f79c00e7dc2b1
gk199 <gillian.kopp@gmail.com>
2024-04-30 01:12:37 +0200

    updating expected BR sensitivity with errors and full calculation implemented

--- CutflowAnalysis/ExpectedLimitsPlot.py
+++ CutflowAnalysis/ExpectedLimitsPlot.py
@@ -31,12 +31,23 @@ cmsLabel = "#scale[1.0]{#bf{CMS} #it{Internal}}"
 cmsLabel2 = "#scale[0.8]{#sqrt{s} = 13.6 TeV}"
 
 # list inputs and number of events in each bin
-inputs = ["mH=125, mS=15", "mH=125, mS=50", "mH=250, mS=120", "mH=350, mS=50", "mH=350, mS=160"]
+inputs = ["mH=125, mS=15", "mH=125, mS=50", "mH=250, mS=120", "mH=350, mS=80", "mH=350, mS=160"]
 
-ctau_pt5 = [-1, -1, -1, 0.00094, -1]
-ctau_1 = [0.135, -1, -1, -1, -1]
-ctau_3 = [-1, 0.0071, -1, -1, -1]
-ctau_10 = [-1, -1, 0.0056, -1, 0.0026]
+overall_acceptance = [2/292437, 382/2943719, 487/2937728, 293/299110, 1043/2946400]
+
+ctau_pt5 = [-1, -1, -1, 293/299110, -1]
+ctau_1 = [2/292437, -1, -1, -1, -1]
+ctau_3 = [-1, 382/2943719, -1, -1, -1]
+ctau_10 = [-1, -1, 487/2937728, -1, 1043/2946400]
+
+n_pass = [2, 382, 487, 293, 1043]
+n_total = [292437, 2943719, 2937728, 299110, 2946400]
+
+# Full calculation results below
+#ctau_pt5 = [-1, -1, -1, 0.00094, -1]
+#ctau_1 = [0.135, -1, -1, -1, -1]
+#ctau_3 = [-1, 0.0071, -1, -1, -1]
+#ctau_10 = [-1, -1, 0.0056, -1, 0.0026]
 
 stack = False
 if stack: hs = ROOT.THStack("hs",  "Expected Limits based on BDT double jet tag; LLP c#tau;Branching Ratio")
@@ -63,10 +74,16 @@ for type in inputs:
         bin3 = 7
         bin4 = 19
 
-    JetsPerCategory[hname].SetBinContent(bin1, ctau_pt5[i]) 
-    JetsPerCategory[hname].SetBinContent(bin2, ctau_1[i]) 
-    JetsPerCategory[hname].SetBinContent(bin3, ctau_3[i]) 
-    JetsPerCategory[hname].SetBinContent(bin4, ctau_10[i]) 
+    JetsPerCategory[hname].SetBinContent(bin1, 3 / (67 * ctau_pt5[i] * 48520)) 
+    if (ctau_pt5[i] > 0): JetsPerCategory[hname].SetBinError(bin1, 3 / (67 * ctau_pt5[i] * 48520) * sqrt(ctau_pt5[i] * (1-ctau_pt5[i])/n_total[i]) * 1/ctau_pt5[i] ) # binomial errors 
+    JetsPerCategory[hname].SetBinContent(bin2, 3 / (67 * ctau_1[i] * 48520)) 
+    if (ctau_1[i] > 0): JetsPerCategory[hname].SetBinError(bin2, 3 / (67 * ctau_1[i] * 48520) * sqrt(ctau_1[i] * (1-ctau_1[i])/n_total[i]) * 1/ctau_1[i] ) 
+    JetsPerCategory[hname].SetBinContent(bin3, 3 / (67 * ctau_3[i] * 48520)) 
+    if (ctau_3[i] > 0): JetsPerCategory[hname].SetBinError(bin3, 3 / (67 * ctau_3[i] * 48520) * sqrt(ctau_3[i] * (1-ctau_3[i])/n_total[i]) * 1/ctau_3[i] ) 
+    JetsPerCategory[hname].SetBinContent(bin4, 3 / (67 * ctau_10[i] * 48520)) 
+    if (ctau_10[i] > 0): JetsPerCategory[hname].SetBinError(bin4, 3 / (67 * ctau_10[i] * 48520) * sqrt(ctau_10[i] * (1-ctau_10[i])/n_total[i]) * 1/ctau_10[i] ) 
+        # print(3 / (67 * ctau_10[i] * 48520) * sqrt(ctau_10[i] * (1-ctau_10[i])/n_total[i]) * 1/ctau_10[i]) # binomial
+        # print(3 / (67 * ctau_10[i] * 48520) * sqrt(1/n_total[i]) * 1/ctau_10[i]) # poisson
     JetsPerCategory[hname].SetMarkerSize(1.5)
     JetsPerCategory[hname].SetMarkerStyle(21)
     JetsPerCategory[hname].GetYaxis().SetRangeUser(0.00005, 1)
@@ -78,8 +95,8 @@ for type in inputs:
     if "LLP" not in type:
         JetsPerCategory[hname].SetFillStyle(3004)
     if not stack:
-        if i == 0: JetsPerCategory[hname].Draw("P PMC")
-        else: JetsPerCategory[hname].Draw("SAME P PMC")
+        if i == 0: JetsPerCategory[hname].Draw("P PMC PLC E1")
+        else: JetsPerCategory[hname].Draw("SAME P PMC PLC E1")
     i += 1
     if stack: hs.Add(JetsPerCategory[hname])
 
