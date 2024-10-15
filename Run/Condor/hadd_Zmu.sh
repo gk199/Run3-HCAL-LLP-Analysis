e81f60aa4b21b6f5724095b42502cbe15fbb39a2
gk199 <gillian.kopp@gmail.com>
2024-10-15 18:45:22 +0200

    submitting v3.9 minituples, adding all scripts

---
+++ Run/Condor/hadd_Zmu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# find-replace v3.9 -> vx
+# find-replace 2024_10_14 -> today's date
+
+echo "Zmu 2023 skim"
+hadd -j /eos/cms/store/group/phys_exotica/HCAL_LLP/MiniTuples/v3.9/minituple_v3.9_Zmu_Run2023Bv1_2024_10_14.root /eos/user/g/gkopp/LLP_Analysis/output_minituples_v3.9_Zmu_2023Bv1_2024_10_14/*.root
+
+hadd -j /eos/cms/store/group/phys_exotica/HCAL_LLP/MiniTuples/v3.9/minituple_v3.9_Zmu_Run2023Cv1_2024_10_14.root /eos/user/g/gkopp/LLP_Analysis/output_minituples_v3.9_Zmu_2023Cv1_2024_10_14/*.root
+
+hadd -j /eos/cms/store/group/phys_exotica/HCAL_LLP/MiniTuples/v3.9/minituple_v3.9_Zmu_Run2023Cv2_2024_10_14.root /eos/user/g/gkopp/LLP_Analysis/output_minituples_v3.9_Zmu_2023Cv2_2024_10_14/*.root
+
+hadd -j /eos/cms/store/group/phys_exotica/HCAL_LLP/MiniTuples/v3.9/minituple_v3.9_Zmu_Run2023Cv3_2024_10_14.root /eos/user/g/gkopp/LLP_Analysis/output_minituples_v3.9_Zmu_2023Cv3_2024_10_14/*.root
+
+hadd -j /eos/cms/store/group/phys_exotica/HCAL_LLP/MiniTuples/v3.9/minituple_v3.9_Zmu_Run2023Cv4_2024_10_14.root /eos/user/g/gkopp/LLP_Analysis/output_minituples_v3.9_Zmu_2023Cv4_2024_10_14/*.root
+
+hadd -j /eos/cms/store/group/phys_exotica/HCAL_LLP/MiniTuples/v3.9/minituple_v3.9_Zmu_Run2023Dv1_2024_10_14.root /eos/user/g/gkopp/LLP_Analysis/output_minituples_v3.9_Zmu_2023Dv1_2024_10_14/*.root
+
+hadd -j /eos/cms/store/group/phys_exotica/HCAL_LLP/MiniTuples/v3.9/minituple_v3.9_Zmu_Run2023Dv2_2024_10_14.root /eos/user/g/gkopp/LLP_Analysis/output_minituples_v3.9_Zmu_2023Dv2_2024_10_14/*.root
