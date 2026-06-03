7b3da60a35fd2c4f734d88b59de6279786b72e1f
gk199 <gillian.kopp@gmail.com>
2026-06-03 19:15:15 +0200

    new options in condor submission

--- Run/Condor/submit_all.sh
+++ Run/Condor/submit_all.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 # Update these!
-output_basedir=/eos/home-g/gkopp/LLP_Analysis/MiniTuples/v5.4/
+output_basedir=/eos/home-g/gkopp/LLP_Analysis/MiniTuples/v5.5/
 proxypath=~/private/x509up_u101898
 
-#output_basedir=/eos/home-g/gkopp/LLP_Analysis/MiniTuples/v5.1/
-#proxypath=~/private/x509up_u101898
-
-systematic=Nominal
-
-for input_file in ../Ntuples_v5/InputFiles_HToSSTo4B_MH*L1triggers_v5.txt; do
+for input_file in ../Ntuples_v5/InputFiles_DisplacedJet_Run2023*_v5.txt; do
 
   filetag=`basename $input_file .txt | sed 's/^InputFiles_//'`
   echo "Submitting job for: $input_files"
-  echo "python3 condor_run.py -i $input_file -o $output_basedir$filetag -p  ~/private/x509up_u101440  -f $filetag$systematic -u $systematic"
-  #python3 condor_run.py -i $input_file -o $output_basedir$filetag -p  ~/private/x509up_u101440  -f $filetag$systematic -u $systematic
+  echo "python3 condor_run.py -i $input_file -o $output_basedir$filetag -p $proxypath  -f $filetag"
+  python3 condor_run.py -i $input_file -o $output_basedir$filetag -p $proxypath  -f $filetag
 
 done
